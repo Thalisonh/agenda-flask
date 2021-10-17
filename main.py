13d617bf83876c91d80e6b21eea551e0910461eb
thalisonh <thalison_henrique@hotmail.com>
2021-10-17 01:02:06 -0300

    create post form

---
+++ main.py
@@ -0,0 +1,62 @@
+import sqlite3
+from flask import Flask, render_template, request, redirect
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        password TEXT,
+        created_at TEXT,
+        updated_at TEXT
+    );
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS contacts (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT,
+    phone TEXT,
+    image TEXT,
+    user_id INTEGER,
+    created_at TEXT,
+    updated_at TEXT
+);
+""")
+
+conn.close()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * from contacts")
+    names = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', names=names)
+
+@app.route("/create", methods=["POST"])
+def create():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone = request.form.get('phone')
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute(
+        'INSERT INTO contacts (name,email,phone) VALUES (?,?,?)', (name, email, phone)
+    )
+
+    conn.commit()
+    conn.close()
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run()
